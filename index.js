55bc1079d1e62bf5a6df13a01b534fa072f181a4
RkzNew <rahamannovel@gmail.com>
2022-11-21 00:07:04 +0600

    add new service CREATE added(Post MEthod)

--- index.js
+++ index.js
@@ -13,66 +13,72 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-  try {
-      const serviceCollection = client.db('foodie').collection('services');
-      const reviewCollection = client.db('foodie').collection('reviews');
+    try {
+        const serviceCollection = client.db('foodie').collection('services');
+        const reviewCollection = client.db('foodie').collection('reviews');
 
-      app.get('/services', async (req, res) => {
-          const query = {}
-          const cursor = serviceCollection.find(query);
-          const services = await cursor.toArray();
-          res.send(services);
-      });
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        });
 
-      app.get('/displayService', async (req, res) => {
-          const query = {}
-          const cursor = serviceCollection.find(query);
-          const services = await cursor.limit(3).toArray();
-          res.send(services);
-      });
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
-      app.get('/services/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: ObjectId(id) };
-          const service = await serviceCollection.findOne(query);
-          res.send(service);
-      });
+        app.get('/displayService', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
 
-
-      // reviews api
-      app.get('/reviews', async (req, res) => {
-          let query = {};
-
-          if (req.query.email) {
-              query = {
-                  email: req.query.email
-              }
-          }
-
-          const cursor = reviewCollection.find(query);
-          const reviews = await cursor.toArray();
-          res.send(reviews);
-      });
-
-      app.post('/reviews', async (req, res) => {
-          const review = req.body;
-          const result = await reviewCollection.insertOne(review);
-          res.send(result);
-      });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
 
+        // reviews api
+        app.get('/reviews', async (req, res) => {
+            let query = {};
 
-      app.delete('/reviews/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: ObjectId(id) };
-          const result = await reviewCollection.deleteOne(query);
-          res.send(result);
-      })
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
 
-  }
-  finally {
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
 
-  }
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+
+
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
 
 }
 
@@ -82,8 +88,8 @@ run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Foodie Service Running');
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Port is running on ${port}`)
-  })
+})
